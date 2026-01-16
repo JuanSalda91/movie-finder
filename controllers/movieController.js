fd78892ac750fbdeb3d1339e01cca98163cf5dac
JuanSalda91 <juansalda9131@gmail.com>
2026-01-16 01:17:08 -0500

    express router and controller functions added

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,1 +1,7 @@
 // IMPORT EXPRESS ROUTER //
+const express = require('express');
+const router = express.Router();
+
+// IMPORT CONTROLLER FUNCTIONS //
+const { searchMovies, getMovieDetails } = require("../controllers/movieController.js");
+
