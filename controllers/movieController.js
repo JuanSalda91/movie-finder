94b6f5f4a8ff985ee210c832411dc3e5f316e1ce
JuanSalda91 <juansalda9131@gmail.com>
2026-01-16 01:19:46 -0500

    routes defined

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -5,3 +5,9 @@ const router = express.Router();
 // IMPORT CONTROLLER FUNCTIONS //
 const { searchMovies, getMovieDetails } = require("../controllers/movieController.js");
 
+// DEFINE MOVIE ROUTES //
+// GET /api/movies/search?title=movieTitle
+router.get("/search", searchMovies);
+
+// GET /api/movies/:id
+router.get("/movies/:id", getMovieDetails);
